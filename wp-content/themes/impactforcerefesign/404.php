68a3b64dd736057cc3b24a7d27cf81c8e4206c8f
Stepan Melykh <melychst@gmail.com>
2024-05-30 16:02:22 +0300

    404

--- wp-content/themes/impactforcerefesign/404.php
+++ wp-content/themes/impactforcerefesign/404.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
-
+<section id="primary" class="page-404 content-area">
+    <div class="container">
+        <h1 style="text-align: center;">404</h1>
+    </div>
+</section><!-- .content-area -->
 <?php get_footer(); ?>
