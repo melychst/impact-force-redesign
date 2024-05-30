08c546b1cdbeecab26799413c4495cdec2f7b1bc
Stepan Melykh <melychst@gmail.com>
2024-05-30 16:17:04 +0300

    404

--- wp-content/themes/impactforcerefesign/404.php
+++ wp-content/themes/impactforcerefesign/404.php
@@ -2,6 +2,7 @@
 <section id="primary" class="page-404 content-area">
     <div class="container">
         <h1 style="text-align: center;">404</h1>
+        <p style="text-align: center;">page not found</p>
     </div>
 </section><!-- .content-area -->
 <?php get_footer(); ?>
