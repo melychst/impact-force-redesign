8ce9e03237b90e8cf58762f0390cd12b8194b708
Stepan Melykh <melychst@gmail.com>
2024-10-21 15:46:45 +0300

    GTAG

--- wp-content/themes/impactforcerefesign/header.php
+++ wp-content/themes/impactforcerefesign/header.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X9ZCBPX5YG"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-X9ZCBPX5YG');
+    </script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
